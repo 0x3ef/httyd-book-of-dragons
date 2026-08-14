e889615680d5354b7cb84dbb58989db38545b816
0x3EFFFC8 <x3r0_sexy@outlook.com>
2026-08-14 12:24:35 -0500

    feat: register auth router

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -4,6 +4,7 @@ from src.images.routes import images_router
 from src.abilities.routes import abilities_router
 from src.distributions.routes import distributions_router
 from src.dragons.routes import dragons_router 
+from src.auth.routes import auth_router 
 from .errors import register_all_errors
 from .middleware import register_middleware 
 
@@ -12,7 +13,7 @@ version_prefix = f"/api/{version}"
 
 app = FastAPI(
         title="Dragon API",
-    version=version,
+        version=version,
 )
 
 register_all_errors(app)
@@ -25,3 +26,4 @@ app.include_router(images_router,prefix=f"{version_prefix}/images",tags=["images
 app.include_router(abilities_router,prefix=f"{version_prefix}/abilities",tags=["abilities"])
 app.include_router(distributions_router,prefix=f"{version_prefix}/distributions",tags=["distributions"])
 app.include_router(dragons_router,prefix=f"{version_prefix}/dragons",tags=["dragons"])
+app.include_router(auth_router,prefix=f"{version_prefix}/auth",tags=["auth"])

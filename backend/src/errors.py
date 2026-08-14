30de6d414478f9023a3ac08ae0c3c6022368f5d7
0x3EFFFC8 <x3r0_sexy@outlook.com>
2026-08-14 12:23:15 -0500

    feat: add error handling for auth routes

--- backend/src/errors.py
+++ backend/src/errors.py
@@ -38,9 +38,55 @@ class DragonNotFound(DragonException):
     """Dragon Not found"""
     pass 
 
+
 class DragonAlreadyExists(DragonException):
     """Dragon already exists"""
     pass 
+
+
+class AccountNotVerified(DragonException):
+    """Account not yet verified"""
+    pass
+
+
+class InvalidToken(DragonException):
+    """User has provided an invalid or expired token"""
+    pass
+
+
+class RevokedToken(DragonException):
+    """User has provided a token that has been revoked"""
+    pass
+
+
+class AccessTokenRequired(DragonException):
+    """User has provided a refresh token when an access token is needed"""
+    pass
+
+
+class RefreshTokenRequired(DragonException):
+    """User has provided an access token when a refresh token is needed"""
+    pass
+
+
+class UserAlreadyExists(DragonException):
+    """User has provided an email for a user who exists during sign up."""
+    pass
+
+
+class InvalidCredentials(DragonException):
+    """User has provided wrong email or password during log in."""
+    pass
+
+
+class InsufficientPermission(DragonException):
+    """User does not have the neccessary permissions to perform an action."""
+    pass
+
+
+class UserNotFound(DragonException):
+    """User Not found"""
+    pass
 
 
 def create_exception_handler(
@@ -127,5 +173,120 @@ def register_all_errors(app: FastAPI) -> None:
                 "message": "A dragon with this name already exists",
                 "error_code": "dragon_already_exists"
                 }
+        ),
+    )
+    
+    app.add_exception_handler(
+        AccountNotVerified,
+        create_exception_handler(
+            status_code=status.HTTP_403_FORBIDDEN,
+            initial_detail={
+                "message": "Account Not verified",
+                "error_code": "account_not_verified",
+                "resolution":"Please check your email for verification details"
+            },
+        ),
+    )
+
+    app.add_exception_handler(
+        UserAlreadyExists,
+        create_exception_handler(
+            status_code=status.HTTP_403_FORBIDDEN,
+            initial_detail={
+                "message": "User with email already exists",
+                "error_code": "user_exists",
+            },
+        ),
+    )
+
+    app.add_exception_handler(
+        UserNotFound,
+        create_exception_handler(
+            status_code=status.HTTP_404_NOT_FOUND,
+            initial_detail={
+                "message": "User not found",
+                "error_code": "user_not_found",
+            },
+        ),
+    )
+    
+    app.add_exception_handler(
+        InvalidCredentials,
+        create_exception_handler(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            initial_detail={
+                "message": "Invalid Email Or Password",
+                "error_code": "invalid_email_or_password",
+            },
+        ),
+    )
+    
+    app.add_exception_handler(
+        InvalidToken,
+        create_exception_handler(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            initial_detail={
+                "message": "Token is invalid Or expired",
+                "resolution": "Please get new token",
+                "error_code": "invalid_token",
+            },
+        ),
+    )
+    
+    app.add_exception_handler(
+        RevokedToken,
+        create_exception_handler(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            initial_detail={
+                "message": "Token is invalid or has been revoked",
+                "resolution": "Please get new token",
+                "error_code": "token_revoked",
+            },
+        ),
+    )
+    
+    app.add_exception_handler(
+        AccessTokenRequired,
+        create_exception_handler(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            initial_detail={
+                "message": "Please provide a valid access token",
+                "resolution": "Please get an access token",
+                "error_code": "access_token_required",
+            },
+        ),
+    )
+    
+    app.add_exception_handler(
+        RefreshTokenRequired,
+        create_exception_handler(
+            status_code=status.HTTP_403_FORBIDDEN,
+            initial_detail={
+                "message": "Please provide a valid refresh token",
+                "resolution": "Please get an refresh token",
+                "error_code": "refresh_token_required",
+            },
+        ),
+    )
+    
+    app.add_exception_handler(
+        UserNotFound,
+        create_exception_handler(
+            status_code=status.HTTP_404_NOT_FOUND,
+            initial_detail={
+                "message": "User not found",
+                "error_code": "user_not_found",
+            },
+        ),
+    )
+
+    app.add_exception_handler(
+        InsufficientPermission,
+        create_exception_handler(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            initial_detail={
+                "message": "You do not have enough permissions to perform this action",
+                "error_code": "insufficient_permissions",
+            },
         ),
     )
